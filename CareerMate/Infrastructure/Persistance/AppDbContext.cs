88e00da0aea262141bc6fa25e14cc419768c44e4
Dhanushka <dhanushka.p@calcey.com>
2024-03-13 20:57:04 +0530

    Implement identity in DB

---
+++ CareerMate/Infrastructure/Persistance/AppDbContext.cs
@@ -0,0 +1,15 @@
+﻿using CareerMate.Models;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace CareerMate.Infrastructure.Persistence
+{
+    public class AppDbContext : IdentityDbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
+        {
+        }
+
+        public DbSet<Test> Test { get; set; }
+    }
+}
