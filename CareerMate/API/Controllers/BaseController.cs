c843da06ebad2a60efff519c176fab77ce8f1aef
Dhanushka <dhanushka.p@calcey.com>
2024-03-14 20:54:38 +0530

    BaseController

---
+++ CareerMate/API/Controllers/BaseController.cs
@@ -0,0 +1,19 @@
+﻿using CareerMate.EndPoints.Handlers;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CareerMate.API.Controllers
+{
+    [ApiController]
+    [Produces("application/json")]
+    [Consumes("application/json")]
+    public class BaseController : ControllerBase
+    {
+        protected ActionResult ToActionResult(BaseResponse response)
+        {
+            return new ObjectResult(response)
+            {
+                StatusCode = response.StatusCode,
+            };
+        }
+    }
+}
