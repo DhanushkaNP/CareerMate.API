a39309fb4182b72b02a90ed1fd13fd85f1385656
Dhanushka Nuwan Premasiri <97797596+DhanushkaNP@users.noreply.github.com>
2024-10-06 11:31:22 +0530

    Allowed any origins

--- CareerMate/Program.cs
+++ CareerMate/Program.cs
@@ -38,7 +38,7 @@ builder.Services.AddCors(options =>
     options.AddPolicy(name: corsPolicy, policy =>
     {
         policy
-            .WithOrigins("http://localhost:3000", "https://gray-field-05e650100.5.azurestaticapps.net")
+            .AllowAnyOrigin()
             .AllowAnyMethod()
             .AllowAnyHeader()
             .AllowCredentials()
